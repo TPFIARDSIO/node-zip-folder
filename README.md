b6bcca98a88e08d6940f94f905988a8525c62d4d
Rayane KADRI <kadrirayane@gmail.com>
2023-01-09 23:49:00 +0100

    Projet fini

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # node-zip-folder
+Module permettant d'archiver un répertoire de manière récursive et pouvoir le restaurer
--- app.js
+++ app.js
@@ -2,22 +2,37 @@ const fs = require('fs');
 const path = require('path');
 const zipper = require("zip-local");
 
-const getDate = () => {
+const packRepertoire =  (pathsrc,pathdest) => {
     let maDate = new Date();
     let annee = maDate.getFullYear();
     let mois = maDate.getMonth()+1;
     let jour = maDate.getDate();
+    let heures = maDate.getHours();
+    let minutes = maDate.getMinutes();
+    let secondes = maDate.getSeconds();
+    if(heures < 10){
+        heures = '0'+mois;
+    }
+    if(minutes < 10){
+        minutes = '0'+minutes;
+    }
+    if(secondes < 10){
+        secondes = '0'+secondes;
+    }
+    if(mois < 10){
+        mois = '0'+mois;
+    }
+    if(jour < 10){
+        jour = '0'+jour;
+    }
+    let timestamp = heures +' H '+minutes+' : '+secondes;
     let date = annee+'-'+mois+'-'+jour;
-    return date;
-}
-
-const packRepertoire =  (pathsrc,pathdest) => {
     const nouveauChemin = path.resolve(pathdest.slice(0,pathdest.indexOf('/')));
-    const nomFichiers = nouveauChemin+'/'+'BUP-'+getDate()+0+'.tar.gz';
-    zipper.sync.zip(pathsrc).compress().save(nouveauChemin+'/'+'BUP-'+getDate()+'.tar.gz');
+    zipper.sync.zip(pathsrc).compress().save(nouveauChemin+'/'+'BUP-'+date+'.tar.gz');
+    creerLog(timestamp,pathdest,date);
 };
 
-const unpackRepertoire = (pathsrc,pathdest) =>{
+const restaure = (pathsrc,pathdest) =>{
     if(!fs.existsSync(pathdest)){
         fs.mkdirSync(pathdest);
         zipper.sync.unzip(pathsrc).save(pathdest);
@@ -43,13 +58,29 @@ const sauvegarde = (pathsrc,pathdest) => {
         }
     }else {
         packRepertoire(pathsrc,pathdest);
+        console.log("Sauvegarde Réussi !")
     }
 }
 
-const sauvegardeMinuter = (pathsrc,pathdest) => {
-    setInterval(sauvegarde(pathsrc,pathdest),5000);
+const creerLog = (timestamp,pathdest,date) => {
+    if(!fs.existsSync('./Meslog/')){
+        fs.mkdirSync('./Meslog/');
+            let logger = fs.createWriteStream('./Meslog/logbup.txt', { flags: 'a'});
+            logger.write('Chemin de la sauvegarde '+path.resolve(pathdest)+'le : '+date+' à '+timestamp+"\r\n");
+    }else{
+            let logger = fs.createWriteStream('./Meslog/logbup.txt'+"\r\n", { flags: 'a'});
+            logger.write('Chemin de la sauvegarde '+path.resolve(pathdest)+'le : '+date+' à '+timestamp+"\r\n");
+    }
+};
+
+const sauvegardeMinuteur = (pathsrc,pathdest,minutes) => {
+    console.log("Appuyez sur CTRL+C pour stopper le programme");
+    let secondes = minutes * 60;
+    setInterval(()=>{sauvegarde(pathsrc,pathdest)},secondes);
 }
 
-sauvegarde('./origine','./destination');
-unpackRepertoire('./destination/BUP-2023-1-8.tar.gz','./BUP-2023-1-8');
-sauvegardeMinuter('./origine','./destination');
+module.exports={
+    sauvegarde,
+    restaure,
+    sauvegardeMinuteur,
+}
